0f2517f46b4a4c535b48f63e988209d53541a513
Aaron Kagan <aaronkagan@yahoo.com>
2024-01-24 12:26:47 -0500

    basic markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
   const [jobs, setJobs] = useState<any>(null);
   const [loading, setLoading] = useState(true);
+  const [activeFilters, setActiveFilters] = useState<string[]>([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -25,11 +27,59 @@ export default function Home() {
     return <Loading />;
   }
 
-  return <Main />;
+  return (
+    <Main
+      jobs={jobs}
+      activeFilters={activeFilters}
+      setActiveFilters={setActiveFilters}
+    />
+  );
 }
 
-function Main() {
-  return <main>Hello World</main>;
+function Main({ jobs, activeFilters, setActiveFilters }) {
+  return (
+    <main>
+      <header>Header</header>
+      <div>
+        {activeFilters.map((activeFilter) => {
+          return <span>{activeFilter}</span>;
+        })}
+        <span>clear</span>
+      </div>
+      {jobs &&
+        jobs.map((job) => {
+          console.log(job);
+          const filters = [job.role, job.level, ...job.tools, ...job.languages];
+          return (
+            <article>
+              <Image
+                width={48}
+                height={48}
+                src={job.logo}
+              />
+              <div>
+                <span>{job.company}</span>
+                {job.new && <span>new!</span>}
+                {job.featured && <span>featured!</span>}
+              </div>
+
+              <h2>{job.position}</h2>
+              <div>
+                <span>{job.postedAt}</span>
+                <span>{job.contract}</span>
+                <span>{job.location}</span>
+              </div>
+              <div className="w-[279px] max-w-[90%] h-[1px] bg-[#B7C4C4]"></div>
+              <div>
+                {filters.map((filter) => {
+                  return <span>{filter}</span>;
+                })}
+              </div>
+            </article>
+          );
+        })}
+    </main>
+  );
 }
 
 function Loading() {
